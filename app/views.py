b1b7220df75cb366ca4adad7cfbd1dc5b7405c1d
IT Administrator <admin@MacBook-Pro.local>
2023-11-09 20:18:37 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Nila1(request):
+    return render(request,'Nila1.html')
+
+def Nila2(request):
+    return render(request,'Nila2.html')
+
+def Nila3(request):
+    return render(request,'Nila3.html')
+
+def Nila4(request):
+    return render(request,'Nila4.html')
